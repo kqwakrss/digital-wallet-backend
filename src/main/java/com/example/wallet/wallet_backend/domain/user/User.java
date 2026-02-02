108385873824deaf7353ae6793f2c03a8eb3a9b3
Maxum <kqwakrss@gmail.com>
2026-02-02 17:32:27 +0200

    add user JPA entity

--- src/main/java/com/example/wallet/wallet_backend/domain/user/User.java
+++ src/main/java/com/example/wallet/wallet_backend/domain/user/User.java
@@ -1,5 +1,24 @@
 package com.example.wallet.wallet_backend.domain.user;
 
 public class User {
-    
+    private String login;
+    private String password;
+    private String email;
+    private UserStatus status;
+    public User(String login){
+        this.login = login;
+        this.status = UserStatus.INACTIVE;
+    }
+    public String getLogin(){
+        return login;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public UserStatus getStatus(){
+        return status;
+    }
 }
