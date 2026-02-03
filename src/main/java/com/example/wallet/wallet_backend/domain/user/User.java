a902e8267fb0d12a5ee796567309ba7f022651b8
Maxum <kqwakrss@gmail.com>
2026-02-03 16:34:53 +0200

    Add initial Flyway migration for users table

--- src/main/java/com/example/wallet/wallet_backend/domain/user/User.java
+++ src/main/java/com/example/wallet/wallet_backend/domain/user/User.java
@@ -1,10 +1,36 @@
 package com.example.wallet.wallet_backend.domain.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "users")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true, length = 50,updatable = false)
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String login;
     private String password;
     private String email;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserStatus status;
+    protected User(){
+
+    }
     public User(String login){
         this.login = login;
         this.status = UserStatus.INACTIVE;
@@ -20,5 +46,8 @@ public class User {
     }
     public UserStatus getStatus(){
         return status;
+    }
+    public long getId(){
+        return id;
     }
 }
